8da8f7e0da625f8afad679b3e104c1a8c5c607a3
Shreya Mahesh <smah0632@uni.sydney.edu.au>
2025-04-12 10:41:16 +1000

    Resolve merge conflict in postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
-=======
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
->>>>>>> 3b5a1765db437dce7de03c8277ebfc9136abe408
